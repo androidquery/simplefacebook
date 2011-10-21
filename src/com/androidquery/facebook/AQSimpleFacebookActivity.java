47615b0311b9f977dc0980a7971f99024f29f9b7
Peter Liu <tinyeeliu@gmail.com>
2011-10-21 06:50:27 -0700

    Initial import.

--- src/com/androidquery/facebook/AQSimpleFacebookActivity.java
+++ src/com/androidquery/facebook/AQSimpleFacebookActivity.java
@@ -9,5 +9,6 @@ public class AQSimpleFacebookActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //dwdw
     }
 }
